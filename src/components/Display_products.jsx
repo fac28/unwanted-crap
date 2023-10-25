aa33d7fad2b7407d26c1331a238340b840e84657
tess-phillips <tess.phillips@me.com>
2023-10-25 14:41:53 +0100

    search bar added

--- src/components/Display_products.jsx
+++ src/components/Display_products.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from 'next/image';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { BasketContext } from '@/context/basket.context';
 import Link from 'next/link';
 //Generating unique keys
@@ -9,32 +9,49 @@ const { v4: uuidv4 } = require('uuid');
 
 export const Display_products = ({ productsInfo }) => {
   const { dispatch } = useContext(BasketContext);
+  const [searchText, setSearchText] = useState('');
 
   const addToBasket = (product) => {
     dispatch({ type: 'ADD', article: product });
   };
+
+  const filteredProducts = productsInfo.filter((product) => {
+    const regex = new RegExp(searchText, 'i'); // 'i' makes the search case-insensitive
+    return regex.test(product.name);
+  });
+
   return (
-    <div className="card">
-      {productsInfo.map((product, index) => (
-        <Link key={uuidv4()} href={`/products/${index + 1}`}>
-          <div className="card-product">
-            <Image
-              priority
-              src={product.image}
-              width={100}
-              height={100}
-              alt={product.name}
-            />
-            <div className="card-product-infos">
-              <h2>{product.name}</h2>
-              <p>£{product.price}</p>
-              <button onClick={() => addToBasket(product)}>
-                Buy
-              </button>
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search by product name"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+      </div>
+      <div className="card">
+        {filteredProducts.map((product, index) => (
+          <Link key={uuidv4()} href={`/products/${index + 1}`}>
+            <div className="card-product">
+              <Image
+                priority
+                src={product.image}
+                width={100}
+                height={100}
+                alt={product.name}
+              />
+              <div className="card-product-infos">
+                <h2>{product.name}</h2>
+                <p>£{product.price}</p>
+                <button onClick={() => addToBasket(product)}>
+                  Buy
+                </button>
+              </div>
             </div>
-          </div>
-        </Link>
-      ))}
-    </div>
+          </Link>
+        ))}
+      </div>
+    </>
   );
 };
