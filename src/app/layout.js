7464519556e7c2ab40a9d5fdf07a4f89cff08f8e
sgroil <laurencesgroi@hotmail.co.uk>
2023-10-26 09:03:52 +0100

    some css

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;500;900&family=Manrope:wght@600&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <BasketContextProvider>{children}</BasketContextProvider>
       </body>
